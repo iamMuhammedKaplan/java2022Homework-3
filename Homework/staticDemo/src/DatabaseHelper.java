a2e9cc31acbf61d2b75e1cde4f801dfc2b25d919
iamMuhammedKaplan <kpln12345@gmail.com>
2022-10-16 01:57:59 +0300

    staticDemo
    yt engin demiroğ java2022 ödev-3

---
+++ Homework/staticDemo/src/DatabaseHelper.java
@@ -0,0 +1,28 @@
+
+public class DatabaseHelper {
+	/*
+	 * 	Inner classlar SOLID Singular-responsibility kuralýný
+	 * çiðnedikleri için kullanýlmasý tercih edilmez.
+	 * 	Onun yerine farklý classlara ayýrarak daha temiz
+	 * bir kod bütünlüðü oluþturmak mümkündür.
+	 */
+	
+	public static class Crud {
+	//Crud = Create-read-update-delete
+		
+		public static void delete() {
+			
+		}
+		public static void update() {
+			
+		}
+		
+	}
+	
+	public static class Connection {
+		
+		public static void createConnection() {
+			
+		}
+	}
+}
---
+++ Homework/staticDemo/src/ProductValidator.java
@@ -0,0 +1,33 @@
+
+public class ProductValidator {
+	
+	static {
+		System.out.println("Static Yapýcý blok çalýþtý.");
+	}
+	
+	public ProductValidator() {
+		System.out.println("Yapýcý blok çalýþtý.");
+	}
+	
+	public static boolean isValid(Product product) {
+		if(product.price > 0 && !product.name.isEmpty()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void bisey() {
+		
+	}
+	
+/*	public static class BaskaBirClass {
+	//inner class
+		
+		public static void sil() {
+			
+		}
+	}
+*/	
+}
